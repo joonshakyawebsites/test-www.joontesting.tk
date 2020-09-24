34e0875ed5f8c838cc2504e39ce68a7d738ab66f
Joon Shakya <joonshakya07@gmail.com>
2020-09-24 22:39:40 +0545

    np

--- mattrab draw over it/js/p5/sketch2.js
+++ mattrab draw over it/js/p5/sketch2.js
@@ -7,18 +7,18 @@ function preload() {
 }
 
 function setup() {
-    createCanvas(document.documentElement.clientWidth, document.documentElement.clientHeight);
-    background(0, 0, 0, 0);
-    errSound.setVolume(0.2);
-    errSound.play();
-    image(errImg, width/2 - 150, height/2 - 60)
-  }
+      createCanvas(document.documentElement.clientWidth, document.documentElement.clientHeight);
+      background(0, 0, 0, 0);
+      errSound.setVolume(0.2);
+      errSound.play();
+      image(errImg, width/2 - 150, height/2 - 60)
+}
 
-  function mouseMoved() {
-        image(errImg, mouseX - 150, mouseY - 10)
-        errSound.play();
-    
-  }
+function mousePressed() {
+      image(errImg, mouseX - 150, mouseY - 10)
+      errSound.play();
+}
+
 function mouseDragged() {
       image(errImg, mouseX - 150, mouseY - 10)
       errSound.play();
--- mattrab draw over it/js/p5/sketch3.js
+++ mattrab draw over it/js/p5/sketch3.js
@@ -1,11 +1,16 @@
 let errImg, errSound;
 
 function preload() {
-    errSound = loadSound('Mr Beast.mp3')
+    song = loadSound('Mr Beast.mp3')
 }
 
 function setup() {
     createCanvas(document.documentElement.clientWidth, document.documentElement.clientHeight);
     background(0, 0, 0, 0);
-    errSound.play();
+}
+
+function mouseMoved() {
+    if (!song.isPlaying()) {
+        song.play();
+    }
 }
